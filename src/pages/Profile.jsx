2be77ff07ce48b5e2e44d4e9212c1298acc1f2b8
Lika <lika.likuna2008@gmail.com>
2024-02-28 04:05:11 +0400

    profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { updateUserDetails, getUserDetails } from "../services/services";
-
+import Button from "../components/Button/Index";
+import { useNavigate } from "react-router-dom";
+import ProfileIcon from "../components/Icons/ProfileIcon";
 const UpdateProfile = () => {
   const [userData, setUserData] = useState({
     first_name: "",
@@ -9,6 +11,7 @@ const UpdateProfile = () => {
   });
 
   const [updatedUserData, setUpdatedUserData] = useState(null);
+  const nav = useNavigate();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -44,44 +47,92 @@ const UpdateProfile = () => {
     }
   }, [updatedUserData]);
 
-  return (
-    <div>
-      <h2>Update Profile</h2>
-      <form className="w-[50%]">
-        <input
-          type="text"
-          name="phone_number"
-          placeholder="Phone Number"
-          value={userData.phone_number}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="first_name"
-          placeholder="First Name"
-          value={userData.first_name}
-          onChange={handleChange}
-        />
-        <input
-          type="text"
-          name="last_name"
-          placeholder="Last Name"
-          value={userData.last_name}
-          onChange={handleChange}
-        />
-        <button type="button" onClick={handleUpdateProfile}>
-          Update Profile
-        </button>
-      </form>
+  const handleLogOut = () => {
+    localStorage.removeItem("accessToken");
+    setUserData({
+      first_name: "",
+      last_name: "",
+      phone_number: "",
+    });
 
-      {updatedUserData && (
-        <div>
-          <h2>Updated User Data</h2>
-          <p>Last Name: {updatedUserData.last_name}</p>
-          <p>First Name: {updatedUserData.first_name}</p>
-          <p>Phone Number: {updatedUserData.phone_number}</p>
+    setTimeout(() => {
+      nav("/");
+    }, 1000);
+  };
+  return (
+    <div className="container pt-[50px]">
+      <div className="flex flex-col gap-y-[20px] ">
+        <div className="flex justify-between items-center border-b-2 pb-[20px] w-full">
+          <div className="flex gap-2 items-center">
+            <ProfileIcon />
+            {updatedUserData && (
+              <p className="font-bold text-[20px] ">
+                გამარჯობა, {updatedUserData.first_name}!
+              </p>
+            )}
+          </div>
+          <div onClick={handleLogOut} className="text-primary cursor-pointer">
+            გამოსვლა
+          </div>
         </div>
-      )}
+        <div className="flex justify-between">
+          {/* Update profile */}
+          <div className="w-[500px] flex flex-col gap-5 ">
+            <p className="font-bold text-[20px]">პროფილის რედაქტირება</p>
+            <form className="flex flex-col gap-y-[20px]">
+              <input
+                type="text"
+                name="phone_number"
+                placeholder="ტელეფონის ნომერი"
+                value={userData.phone_number}
+                onChange={handleChange}
+                className="focus:outline-none font-bold text-base w-full bg-light-grey px-[16px] py-[16px] rounded-[16px]"
+              />
+              <input
+                type="text"
+                name="first_name"
+                placeholder="სახელი"
+                value={userData.first_name}
+                onChange={handleChange}
+                className="focus:outline-none font-bold text-base w-full bg-light-grey px-[16px] py-[16px] rounded-[16px]"
+              />
+              <input
+                type="text"
+                name="last_name"
+                placeholder="გვარი"
+                value={userData.last_name}
+                onChange={handleChange}
+                className="focus:outline-none font-bold text-base w-full bg-light-grey px-[16px] py-[16px] rounded-[16px]"
+              />
+              <Button
+                onClick={handleUpdateProfile}
+                children="განახლება"
+                className="text-white bg-primary w-full"
+              />
+            </form>
+          </div>
+          {/* Updated profile */}
+          {updatedUserData && (
+            <div className="w-[500px] flex flex-col gap-5">
+              <h2 className="font-bold text-[20px]">განახლებული ინფორმაცია</h2>
+              <div className="bg-light-grey rounded-[16px] p-[20px] flex flex-col gap-3">
+                <p>
+                  <span className="font-bold">ტელეფონის ნომერი:</span>{" "}
+                  {updatedUserData.phone_number}
+                </p>
+                <p>
+                  <span className="font-bold">სახელი:</span>{" "}
+                  {updatedUserData.first_name}
+                </p>
+                <p>
+                  <span className="font-bold">გვარი:</span>{" "}
+                  {updatedUserData.last_name}
+                </p>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
